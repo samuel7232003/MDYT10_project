99274877ca3e6755dc896f0ec5c9017975f54617
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2025-01-03 13:17:14 +0700

    [fix]

--- admin/frontend2/webpack.config.js
+++ admin/frontend2/webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     new Dotenv({
         path: './some.other.env' // default is .env
     })
-  ]
+  ],
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+        exclude: [/node_modules/], // Bỏ qua node_modules để tránh cảnh báo về source map
+      },
+    ],
+  },
 };
